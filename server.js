07ef53ef6473685b43667b594301b41248b8db66
Santiago Alvarez <salvarezd3@eafit.edu.co>
2024-11-23 14:36:59 -0500

    Corregida configuración de ES modules

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-require('dotenv').config;
-import { listen } from './app';
+import { config } from 'dotenv';
+import app from './app.js';
+
+config(); // Inicializa dotenv correctamente
+
 const PORT = process.env.PORT || 3000;
 
 //inicializa el server
-listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
